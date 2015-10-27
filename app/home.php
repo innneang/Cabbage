0fe724d238a82639c44712267c1899858e175135
Siwat Techavoranant <saengthubthim@gmail.com>
2015-10-27 20:23:17 +0700

    Add conceptual home.php

---
+++ app/home.php
@@ -0,0 +1,9 @@
+<?php
+/* home.php
+ * 
+ * The only script that user needs to access.
+ *
+ * Simple webpage that display search box to user
+ * and use ajax to display the result to the user
+ * with footer containing "About" and "Contact" link/modal
+ */
