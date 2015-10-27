cd8c0bdd6a12902ade98a5a033081ef4691e5dae
Siwat Techavoranant <saengthubthim@gmail.com>
2015-10-27 20:31:16 +0700

    Delete home.php

--- app/home.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* home.php
- * 
- * Main page.
- *
- * Simple webpage that display search box to user
- * and use ajax to display the result to the user
- * with footer containing "About" and "Contact" link/modal
- */
