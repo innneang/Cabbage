c622ceaa47e91bbfaebcef9bcb80289819081850
Siwat Techavoranant <saengthubthim@gmail.com>
2015-10-27 20:34:13 +0700

    Add conceptual view/header.php

---
+++ app/view/header.php
@@ -0,0 +1,13 @@
+<?php
+/* header.php
+ * 
+ * HTML Header
+ *
+ * This script provide the metadata, embed required library, make navigation bar (if any)
+ *
+ * Usage: Header::create('-page title here-');
+ */
+ 
+ class Header {
+ 
+ }
