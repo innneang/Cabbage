9471643692abbddbefbd3752eb3213f27215be91
Siwat Techavoranant <saengthubthim@gmail.com>
2015-10-27 20:18:32 +0700

    Add conceptual search.php

---
+++ app/search.php
@@ -0,0 +1,7 @@
+<?php
+/* search.php
+ * 
+ * Input: POST containing ingredients in array form
+ * Process: Search database for recipe using entered ingredients
+ * Output: Display recipe list to user
+ */
