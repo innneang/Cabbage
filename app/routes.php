e8ec72a295debe5118838c7fd16c68443c8475fa
Siwat Techavoranant <saengthubthim@gmail.com>
2015-10-28 18:07:11 +0700

    Delete routes.php

--- app/routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* routes.php
- *
- * First script of the process, all access pass thru here.
- *
- * Route a user to the script/page based on the URL.
- */
-
-define("ROUTED", true, true); //Define constant "ROUTED" to tell other scripts that user request has past here, not direct access.
