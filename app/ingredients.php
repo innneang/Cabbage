38f6112bd379590ec726eb1136809179842a54f3
iinnn <in@iinnn.net>
2015-10-28 20:47:37 +0700

    Revert "Update ingredients.php"
    This reverts commit 7a88516b91cd806f2ec171c66a56c7fc8b5aa7b7.

--- app/ingredients.php
+++ app/ingredients.php
@@ -1,15 +1,2 @@
 <?php
 // echo all ingredient in JSON
-require('config.php');
-$db=getPDO();
-$ing=$db->query('SELECT ingredient FROM recipe');
-$ind=$ing->fetch(PDO::FETCH_ASSOC);
-$ins=array();
-foreach ($ind['ingredient'] as $inv) {
-  $ina=explode(',',$inv);
-  foreach ($ina as $inav) {
-    $ins[]=$inav;
-  }
-}
-echo json_encode($ins);
-?>
