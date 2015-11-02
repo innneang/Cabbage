ac8de4a95ff012250508c46e3a4c7e001dbbfc0c
Siwat Techavoranant <saengthubthim@gmail.com>
2015-11-02 14:33:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
-<!-- Search page -->
 <?php
-require dirname(__FILE__) . '/app/core/AltoRouter.php';
-$router = new AltoRouter();
-$router->map( 'GET', '/', function() {
-    require('app/searchpage/index.php');
-});
-
+require('app/searchpage/index.php');
 ?>
