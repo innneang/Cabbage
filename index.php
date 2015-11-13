adb21e7c672292bff899cd5c2c9463e10b11d0c7
Siwat Techavoranant <saengthubthim@gmail.com>
2015-11-13 10:22:49 +0700

    Reuse old codes

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
 <?php
-echo 'fuck';
-//require( dirname(__FILE__) . 'app/searchpage/index.php');
+/* Index file
+ * 
+ * Search page goes here
+ */
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.min.css">
+</head>
+<body>
+	<h1>Hello, world!</h1>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ search.php
@@ -0,0 +1,18 @@
+<?php
+/* search.php
+ * 
+ * Input: POST containing ingredients in array form ("keyword")
+ * Process: Search database for recipe using entered ingredients
+ * Output: Display recipe list to user
+ */
+require('config.php');
+$db=getPDO();
+$ing=$db->prepare('SELECT * FROM recipe WHERE MATCH (ingredient) AGAINST (:keyword)');
+$ing->bindParam(':keyword',$_POST['keyword']);
+if ($ing->execute()) {
+  $ind=$ing->fetch(PDO::FETCH_ASSOC);
+  echo var_dump($ind);
+  //More code here
+} else {
+  echo 'ERROR'; //TODO: Add more user-friendly error page
+}
