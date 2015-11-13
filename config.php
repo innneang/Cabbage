470e8c016b5d37b85f154e15017dd5abe3c410dc
Siwat Techavoranant <saengthubthim@gmail.com>
2015-11-13 10:45:20 +0700

    Add config.php and index.php

---
+++ config.php
@@ -0,0 +1,50 @@
+<?php
+/* =======================================================================
+ * Config file
+ * All shared variable should be here
+ * =======================================================================
+*/
+
+
+/*
+|--------------------------------------------------------------------------
+| Base Site URL
+|--------------------------------------------------------------------------
+|
+| URL to your root. Typically this will be your base URL,
+| WITH a trailing slash:
+|
+|	http://example.com/
+*/
+$config['base_url'] = 'http://cabbage.iinnn.net';
+
+/*
+|--------------------------------------------------------------------------
+| Site Name
+|--------------------------------------------------------------------------
+|
+| Site name, for example, Cabbage
+*/
+$config['name'] = 'Cabbage';
+
+/*
+|--------------------------------------------------------------------------
+| MySQL Database Credential
+|--------------------------------------------------------------------------
+*/
+$config['mysql']['db'] = 'admin_fridgerecipe';
+$config['mysql']['username'] = 'admin_recipe';
+$config['mysql']['password'] = 'BBJVvgJr0T';
+$config['mysql']['host'] = 'iinnn.net'; //IP Address: 128.199.85.229
+
+
+	function getPDO() {
+		try {
+			$dbh = new PDO('mysql:dbname='.$config['mysql']['db'].';host='.$config['mysql']['host'], $config['mysql']['username'], $config['mysql']['password']);
+		} catch (PDOException $e) {
+			$dbh = false;
+			echo 'Connection failed: ' . $e->getMessage();
+		}
+		return $dbh;
+	}
+
--- index.php
+++ index.php
@@ -2,21 +2,24 @@
 /* Index file
  * 
  * Search page goes here
+ *
+ * This page use BOOTSTRAP v4.0.0-alpha! ::::: http://v4-alpha.getbootstrap.com/
  */
-
+require('config.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta http-equiv="x-ua-compatible" content="ie=edge">
-
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.min.css">
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta http-equiv="x-ua-compatible" content="ie=edge" />
+	<title><?=$config['name']?></title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.min.css" />
 </head>
 <body>
+	<main class="container">
 	<h1>Hello, world!</h1>
-
+	</main>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/js/bootstrap.min.js"></script>
 </body>
