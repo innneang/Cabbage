9d298b9273da430a0960ffec2dce9589f2e1dba0
iinnn <iinnn@users.noreply.github.com>
2015-11-16 15:51:21 +0700

    Update search.php

--- search.php
+++ search.php
@@ -17,8 +17,17 @@ $db=getPDO();
 $ing=$db->prepare('SELECT * FROM recipe WHERE MATCH (ingredient) AGAINST (:keyword)');
 $ing->bindParam(':keyword',$keyw);
 if ($ing->execute()) {
-  $ind=$ing->fetch(PDO::FETCH_ASSOC);
-  echo var_dump($ind);
+  $ind=$ing->fetchAll();
+  foreach ($ind as $inf) { ?>
+   <div class="card">
+  <img class="card-img-top" data-src="holder.js/100%x180/" alt="Card image cap">
+  <div class="card-block">
+    <h4 class="card-title"><?=$inf['name']?></h4> 
+    <p class="card-text"></p>
+    <a href="#" class="btn btn-primary">Button</a>
+  </div>
+</div>
+  <?php }
   //More code here
 } else {
   echo 'ERROR'; //TODO: Add more user-friendly error page
