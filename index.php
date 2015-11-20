f71cb5a0348e71d1ac59ada1895cdfdfb628ba85
Siwat Techavoranant <saengthubthim@gmail.com>
2015-11-20 23:18:17 +0700

    Style typeahead

--- index.php
+++ index.php
@@ -12,49 +12,59 @@ $background = array('https://images.unsplash.com/photo-1428515613728-6b4607e4436
 	<link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 	<link href="http://pingendo.github.io/pingendo-bootstrap/themes/default/bootstrap.css" rel="stylesheet" type="text/css">
 	<title><?=$config['name']?></title>
-</head>
-<body>
-	<div class="cover">            
-		<div class="cover-image" style="background-image: url('<?=$background[array_rand($background)]?>');"></div>
+	<style>
+		.tt-input, .tt-hint {width: 396px;height: 30px;padding: 8px 12px;font-size: 24px;line-height: 30px;border: 2px solid #ccc;border-radius: 8px;
+			outline: none;}
+			.tt-input {box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);}
+			.tt-hint {color: #999;}
+			.tt-menu {width: 422px;margin-top: 12px;padding: 8px 0;background-color: #fff;border: 1px solid #ccc;border: 1px solid rgba(0, 0, 0, 0.2);border-radius: 8px;box-shadow: 0 5px 10px rgba(0,0,0,.2);}
+			.tt-suggestion {padding: 3px 20px;font-size: 18px;line-height: 24px;}
+			.tt-suggestion.tt-cursor {color: #fff;background-color: #0097cf;}
+			.tt-suggestion p {margin: 0;}
+		</style>
+	</head>
+	<body>
+		<div class="cover">            
+			<div class="cover-image" style="background-image: url('<?=$background[array_rand($background)]?>');"></div>
 
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12 text-center">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 text-center">
 
-					<h1 style="text-shadow: 2px 2px 5px grey;" class="text-inverse">มื้อนี้กินอะไรดี?</h1>
-					<p style="text-shadow: 2px 2px 5px grey;" class="text-inverse">กรุณาป้อนข้อมูลวัตถุดิบที่มี</p>
-					<main class="container">
-						<div class='row'>
-							<div id="ingredient" class="col-md-offset-3 col-md-6">
+						<h1 style="text-shadow: 2px 2px 5px grey;" class="text-inverse">มื้อนี้กินอะไรดี?</h1>
+						<p style="text-shadow: 2px 2px 5px grey;" class="text-inverse">กรุณาป้อนข้อมูลวัตถุดิบที่มี</p>
+						<main class="container">
+							<div class='row'>
+								<div id="ingredient" class="col-md-offset-3 col-md-6">
 
+								</div>
 							</div>
-						</div>
-						<form onsubmit="addIngredient();return false;"><div class="text-center">
-							<div id="the-basics" class="row">
-								<div class="col-md-6 col-md-offset-3"><input class="form-control typeahead fullwidth" type="text" id="input" placeholder="Ingredient" size="50" /></div>
+							<form onsubmit="addIngredient();return false;"><div class="text-center">
+								<div id="the-basics" class="row">
+									<div class="col-md-6 col-md-offset-3"><input class="form-control typeahead fullwidth" type="text" id="input" placeholder="Ingredient" size="50" /></div>
 
-								<div class='col-md-1 visible-lg-*' style="visibility: hidden;"><input type="submit" class="btn btn-secondary" value="เพิ่ม" /></div>
-								<br>
-								<br>
+									<div class='col-md-1 visible-lg-*' style="visibility: hidden;"><input type="submit" class="btn btn-secondary" value="เพิ่ม" /></div>
+									<br>
+									<br>
+								</div>
 							</div>
+							<button type="button" class="btn btn-primary" id="submit">ค้นหา</button>
+						</div></form>
+						<div id="result">
+
+
 						</div>
-						<button type="button" class="btn btn-primary" id="submit">ค้นหา</button>
-					</div></form>
-					<div id="result">
-
-
-					</div>
-				</main>
+					</main>
+				</div>
 			</div>
-		</div>
 
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/js/bootstrap.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/typeahead.js/0.11.1/typeahead.jquery.min.js"></script>
-		<script>
-			var substringMatcher = function(strs) {
-				return function findMatches(q, cb) {
-					var matches, substringRegex;
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/js/bootstrap.min.js"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/typeahead.js/0.11.1/typeahead.jquery.min.js"></script>
+			<script>
+				var substringMatcher = function(strs) {
+					return function findMatches(q, cb) {
+						var matches, substringRegex;
             matches = []; // an array that will be populated with substring matches
         substrRegex = new RegExp(q, 'i'); // regex used to determine if a string contains the substring `q`
         $.each(strs, function(i, str) {
